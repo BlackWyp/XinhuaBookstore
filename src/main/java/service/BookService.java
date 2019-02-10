0fc23516773f7cad53856a3de19ba008ac840587
JOJO <743597311@qq.com>
2019-02-10 16:36:22 +0800

    2019年2月10日 16:35:03 增加了BookDao的增删改查

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,5 +1,23 @@
 package service;
 
-public class BookService {
+import java.util.List;
 
+import bean.Book;
+import dao.BookDao;
+
+public class BookService {
+	public List<Book> getAllBooks() {
+		BookDao bookDao=new BookDao();
+		List<Book> books=bookDao.getAllBooks();
+		return books;
+	}
+	
+	public List<Book> getBooksByName(String name) {
+		BookDao bookDao=new BookDao();
+		return bookDao.getBooksByName(name);
+	}
+	public Book getBookById(Integer id) {
+		BookDao bookDao=new BookDao();
+		return bookDao.getBookById(id);
+	}
 }
--- src/main/java/servlet/BookServlet.java
+++ src/main/java/servlet/BookServlet.java
@@ -2,66 +2,82 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Book;
+import service.BookService;
+
 public class BookServlet extends HttpServlet {
-
-	/**
-		 * The doGet method of the servlet. <br>
-		 *
-		 * This method is called when a form has its tag value method equals to get.
-		 * 
-		 * @param request the request send by the client to the server
-		 * @param response the response send by the server to the client
-		 * @throws ServletException if an error occurred
-		 * @throws IOException if an error occurred
-		 */
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		doPost(request, response);
 	}
 
-	/**
-		 * The doPost method of the servlet. <br>
-		 *
-		 * This method is called when a form has its tag value method equals to post.
-		 * 
-		 * @param request the request send by the client to the server
-		 * @param response the response send by the server to the client
-		 * @throws ServletException if an error occurred
-		 * @throws IOException if an error occurred
-		 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the POST method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=UTF-8");
+		String type=request.getParameter("type");
+		BookService bookService = new BookService();
+		List<Book> books=null;
+		Book book=null;
+		String name=null;
+		String id=null;
+		
+		if(type!=null) {
+			switch (type) {
+			/*前台*/
+			case "getAllBooks":
+				books=bookService.getAllBooks();
+				request.setAttribute("books", books);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			case "getBooksByName":
+				name=request.getParameter("name");
+				books=bookService.getBooksByName(name);
+				request.setAttribute("books", books);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			case "getBookById":
+				id=request.getParameter("id");
+				book=bookService.getBookById(Integer.parseInt(id));
+				request.setAttribute("book", book);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			/*后台*/
+			case "getAllBooksForManager":
+				books=bookService.getAllBooks();
+				request.setAttribute("books", books);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			case "getBooksByNameForManager":
+				name=request.getParameter("name");
+				books=bookService.getBooksByName(name);
+				request.setAttribute("books", books);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			case "getBookByIdForManager":
+				id=request.getParameter("id");
+				book=bookService.getBookById(Integer.parseInt(id));
+				request.setAttribute("book", book);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				break;
+			case "update":
+				
+				break;
+			case "deleteById":
+				id=request.getParameter("id");
+				
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 }
