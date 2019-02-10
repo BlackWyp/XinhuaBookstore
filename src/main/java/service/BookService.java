483c36be4974194c2045a9f310caeb3577b00ee2
JOJO <743597311@qq.com>
2019-02-10 20:48:19 +0800

    2019年2月10日 20:47:57 完成了Book相关的后端功能

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -7,17 +7,35 @@ import dao.BookDao;
 
 public class BookService {
 	public List<Book> getAllBooks() {
-		BookDao bookDao=new BookDao();
-		List<Book> books=bookDao.getAllBooks();
+		BookDao bookDao = new BookDao();
+		List<Book> books = bookDao.getAllBooks();
 		return books;
 	}
-	
+
 	public List<Book> getBooksByName(String name) {
-		BookDao bookDao=new BookDao();
+		BookDao bookDao = new BookDao();
 		return bookDao.getBooksByName(name);
 	}
+
 	public Book getBookById(Integer id) {
-		BookDao bookDao=new BookDao();
+		BookDao bookDao = new BookDao();
 		return bookDao.getBookById(id);
+	}
+
+	public void deleteBookById(Integer id) {
+		BookDao bookDao = new BookDao();
+		bookDao.deleteBookById(id);
+	}
+
+	public void addBook(String categoryName, String bookName, String imgUrl, String caption, String introduction,
+			double price) {
+		BookDao bookDao = new BookDao();
+		bookDao.addBook(categoryName, bookName, imgUrl, caption, introduction, price);
+	}
+
+	public void updateBook(Integer id, String categoryName, String bookName, String imgUrl, String caption,
+			String introduction, double price) {
+		BookDao bookDao = new BookDao();
+		bookDao.updateBook(id, categoryName, bookName, imgUrl, caption, introduction, price);
 	}
 }
--- src/main/java/servlet/BookServlet.java
+++ src/main/java/servlet/BookServlet.java
@@ -14,7 +14,7 @@ import bean.Book;
 import service.BookService;
 
 public class BookServlet extends HttpServlet {
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
@@ -22,57 +22,80 @@ public class BookServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=UTF-8");
-		String type=request.getParameter("type");
+		String type = request.getParameter("type");
 		BookService bookService = new BookService();
-		List<Book> books=null;
-		Book book=null;
-		String name=null;
-		String id=null;
-		
-		if(type!=null) {
+		List<Book> books = null;
+		Book book = null;
+		String name = null;
+		String id = null;
+		Integer bookId = null;
+		String categoryName = null;
+		String bookName = null;
+		String imgUrl = null;
+		String caption = null;
+		String introduction = null;
+		double price = 0;
+		if (type != null) {
 			switch (type) {
-			/*前台*/
+			/* 前台 */
 			case "getAllBooks":
-				books=bookService.getAllBooks();
+				books = bookService.getAllBooks();
 				request.setAttribute("books", books);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
 			case "getBooksByName":
-				name=request.getParameter("name");
-				books=bookService.getBooksByName(name);
+				name = request.getParameter("name");
+				books = bookService.getBooksByName(name);
 				request.setAttribute("books", books);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
 			case "getBookById":
-				id=request.getParameter("id");
-				book=bookService.getBookById(Integer.parseInt(id));
+				id = request.getParameter("id");
+				book = bookService.getBookById(Integer.parseInt(id));
 				request.setAttribute("book", book);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
-			/*后台*/
+
+			/* 后台 */
 			case "getAllBooksForManager":
-				books=bookService.getAllBooks();
+				books = bookService.getAllBooks();
 				request.setAttribute("books", books);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
 			case "getBooksByNameForManager":
-				name=request.getParameter("name");
-				books=bookService.getBooksByName(name);
+				name = request.getParameter("name");
+				books = bookService.getBooksByName(name);
 				request.setAttribute("books", books);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
 			case "getBookByIdForManager":
-				id=request.getParameter("id");
-				book=bookService.getBookById(Integer.parseInt(id));
+				id = request.getParameter("id");
+				book = bookService.getBookById(Integer.parseInt(id));
 				request.setAttribute("book", book);
-				getServletContext().getRequestDispatcher("xxx.jsp").forward(request,response);
+				getServletContext().getRequestDispatcher("xxx.jsp").forward(request, response);
 				break;
-			case "update":
-				
+			case "updateBook":
+				bookId = Integer.parseInt(request.getParameter("id"));
+				categoryName = request.getParameter("categoryName");
+				bookName = request.getParameter("bookName");
+				imgUrl = request.getParameter("imgUrl");
+				caption = request.getParameter("caption");
+				introduction = request.getParameter("introduction");
+				price = Double.parseDouble(request.getParameter("price"));
+				bookService.updateBook(bookId, categoryName, bookName, imgUrl, caption, introduction, price);
 				break;
 			case "deleteById":
-				id=request.getParameter("id");
-				
+				id = request.getParameter("id");
+				bookService.deleteBookById(Integer.parseInt(id));
+				break;
+			case "addBook":
+				categoryName = request.getParameter("categoryName");
+				bookName = request.getParameter("bookName");
+				imgUrl = request.getParameter("imgUrl");
+				caption = request.getParameter("caption");
+				introduction = request.getParameter("introduction");
+				price = Double.parseDouble(request.getParameter("price"));
+				bookService.addBook(categoryName, bookName, imgUrl, caption, introduction, price);
 				break;
 			default:
 				break;
